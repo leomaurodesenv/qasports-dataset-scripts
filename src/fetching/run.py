89a4b8cc40e691f4ef9edb980f90246d9091fc87
leomaurodesenv <leo.mauro.desenv@gmail.com>
2024-11-12 22:11:56 -0300

    refactor(fetching): update run module

--- src/fetching/run.py
+++ src/fetching/run.py
@@ -1,24 +1,8 @@
-from ..module import OUTPUT_PATH, RAW_HTML_PATH
+from ..module import OUTPUT_PATH, RAW_HTML_PATH, TESTING, wiki_pages
 from .module import fetch_all_html
 
-TESTING = True
-fetching_pages = [
-    {
-        "sport_name": "basketball",
-        "csv_name": "basketball-links.csv"
-    },
-    {
-        "sport_name": "football",
-        "csv_name": "football-links.csv"
-    },
-    {
-        "sport_name": "americanfootball",
-        "csv_name": "americanfootball-links.csv"
-    },
-]
-
 # fecthing the URLs
-for wiki_page in fetching_pages:
+for wiki_page in wiki_pages:
     sport_name, csv_name = wiki_page["sport_name"], wiki_page["csv_name"]
     fetch_all_html(
         links_file=(OUTPUT_PATH / csv_name),
