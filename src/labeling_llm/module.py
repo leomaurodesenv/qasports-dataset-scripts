a966ec5618e16843756e8639f004eab22bc40ba5
leomaurodesenv <leo.mauro.desenv@gmail.com>
2025-07-19 19:01:51 -0300

    feat(labeling): building labeling validation dataset

--- src/labeling_llm/module.py
+++ src/labeling_llm/module.py
@@ -1,41 +1,36 @@
 """Module to labeling the question-answering dataset"""
 
+import ast
 import torch
+import pandas as pd
+from tqdm import tqdm
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
+# Constants
 MODEL_SHORT_NAME = "qwen"
-MODEL_NAME = "Qwen/Qwen2.5-3B-Instruct"  # Choose one model:
-# MODEL_OPTIONS = [
-#     "Qwen/Qwen2.5-3B-Instruct",
-#     "Qwen/Qwen2.5-7B-Instruct",
-#     "Qwen/Qwen2.5-14B-Instruct",
-#     "Qwen/Qwen2.5-72B-Instruct"
+MODEL_NAME = "Qwen/Qwen2.5-3B-Instruct"
+# Sample of models
+#   "Qwen/Qwen2.5-3B-Instruct"
+#   "Qwen/Qwen2.5-7B-Instruct"
+#   "Qwen/Qwen2.5-14B-Instruct"
+#   "Qwen/Qwen2.5-72B-Instruct"
 
-
-#               MODEL LOADING
-
+# Loading LLM
 tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME, trust_remote_code=True)
-
-
 model = AutoModelForCausalLM.from_pretrained(
     MODEL_NAME, device_map="auto", trust_remote_code=True
 ).eval()
 
 
-#               FUNCTIONS
-
-
 def is_question_valid(question: str, context: str, answer: str) -> str:
     """
     Evaluates whether a question makes sense within the given context.
-
-    Parameters:
-    - question (str): The question to validate.
-    - context (str): The context of the question.
-    - answer (str): The answer provided for that question.
-
+    Args:
+        question (str): The question to validate.
+        context (str): The context of the question.
+        answer (str): The answer provided for that question.
     Returns:
-    - str: "1" if valid, "2" if the subject is not covered in the context.
+        str: "1" if valid, "2" if the subject is not covered in the context.
     """
     prompt = f"""
 You are an evaluator. Given a context, a question, and an answer, classify the question by selecting one of the options below.
@@ -61,14 +56,12 @@ Respond with only the number (1 or 2). Do not include explanations.
 def is_answer_correct(question: str, context: str, answer: str) -> str:
     """
     Evaluates whether the provided answer is correct according to the context.
-
-    Parameters:
-    - question (str): The question being asked.
-    - context (str): The context in which to evaluate the answer.
-    - answer (str): The answer to verify.
-
+    Args:
+        question (str): The question being asked.
+        context (str): The context in which to evaluate the answer.
+        answer (str): The answer to verify.
     Returns:
-    - str: "1" if correct, "2" if incorrect but the answer is found in the context.
+        str: "1" if correct, "2" if incorrect but the answer is found in the context.
     """
     prompt = f"""
 You are an evaluator. Given a context, a question, and a provided answer, classify the answer selecting one option.
@@ -91,7 +84,45 @@ Return only the number (1 or 2). Do not include any explanation.
     return answer.split("any explanation.")[-1].strip()
 
 
-def labeling(input_file: str, output_file: str, test: bool = False):
+def labeling(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Labeling the question-answering dataset.
+    Args:
+        df (pd.DataFrame): The dataset to label.
+    Returns:
+        pd.DataFrame: The labeled dataset.
+    """
+    labeling_results = []
+
+    for _, row in tqdm(
+        df.iterrows(), desc=f"Labeling ({MODEL_SHORT_NAME})", total=len(df)
+    ):
+        question = row["question"]
+        context = row["context"]
+        answer_text = (
+            ast.literal_eval(row["answer"])
+            if isinstance(row["answer"], str)
+            else row["answer"]
+        )["text"]
+
+        valid_q = is_question_valid(question, context, answer_text)
+        correct_a = is_answer_correct(question, context, answer_text)
+
+        labeling_results.append(
+            {
+                "qa_id": row["qa_id"],
+                "question": question,
+                "answer": answer_text,
+                "context": context,
+                "is_question_valid": valid_q,
+                "is_answer_correct": correct_a,
+            }
+        )
+
+    return pd.DataFrame(labeling_results)
+
+
+def labeling_from_file(input_file: str, output_file: str, test: bool = False):
     """
     Labeling the question-answering dataset.
     Args:
@@ -99,4 +130,8 @@ def labeling(input_file: str, output_file: str, test: bool = False):
         output_file (str): The output
         test (bool): testing parameter
     """
-    pass
+    df = pd.read_csv(input_file, sep=",")
+    if test:
+        df = df.head(5)
+    df = labeling(df)
+    print(df)
--- src/labeling_llm/run.py
+++ src/labeling_llm/run.py
@@ -1,5 +1,5 @@
 from ..module import OUTPUT_PATH, TESTING, wiki_pages
-from .module import MODEL_SHORT_NAME, labeling
+from .module import MODEL_SHORT_NAME, labeling_from_file
 
 
 # labeling dataset
@@ -8,11 +8,12 @@ for wiki_page in wiki_pages:
         wiki_page["labeling_name"],
         wiki_page["sampling_name"],
     )
-    labeling(
+    labeling_from_file(
         input_file=(OUTPUT_PATH / sampling_name),
         output_file=(OUTPUT_PATH / "labeling" / MODEL_SHORT_NAME / labeling_name),
         test=TESTING,
     )
+    break
 
 
 # import ast
